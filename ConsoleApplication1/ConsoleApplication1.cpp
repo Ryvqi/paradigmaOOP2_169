b8359d173cafc65679b3d503f38b06743e916d8d
Ryvqi <rifqikhabibi03@gmail.com>
2023-05-29 15:56:28 +0700

    return 0

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -16,9 +16,24 @@ public:
 	}
 };
 
-class Lia : public seseorang {
+class lia : public seseorang {
 public:
 	void pesan() {
 		cout << "Pesan dari Lia" << endl;
 	}
 };
+
+int main() {
+	seseorang* obyek;
+	joko a;
+	lia b;
+
+	obyek = &a;
+	obyek->pesan();
+	obyek = &b;
+	obyek->pesan();
+	//a.seseorang::pesan();
+	//b.seseorang::pesan();
+
+	return 0;
+}
