84aae0e548daf22e3d48a7bfad1d60e2547c8b82
Ryvqi <rifqikhabibi03@gmail.com>
2023-05-29 14:51:09 +0700

    class orang

--- paradigmaOOP2_169/paradigmaOOP2_169.cpp
+++ paradigmaOOP2_169/paradigmaOOP2_169.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+class orang {
+	string nama;
+
+	orang(string pNama) :
+		nama(pNama) {
+		cout << "orang dibuat\n" << endl;
+	}
+	~orang() {
+		cout << "orang dihapus\n" << endl;
+	}
+};
